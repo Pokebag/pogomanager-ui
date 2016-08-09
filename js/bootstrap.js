c260e52c0023ecb7c4bc48710e984e9db1989698
tpeebles <trezy.peebles@gettyimages.com>
2016-08-09 17:45:14 -0500

    Add the poketable function for debugging

--- js/bootstrap.js
+++ js/bootstrap.js
@@ -23,3 +23,33 @@ $(document).ready(function () {
 
   app.start()
 })
+
+window.poketable = function (fields) {
+  let pokemon = Backbone.Radio.channel('application').request('pokemon').toJSON()
+
+  let poketable = []
+
+  pokemon.forEach(mon => {
+    let tablemon = {}
+
+    if (fields) {
+      fields.forEach(field => {
+        let splitFields = field.split('.')
+        let value = mon
+
+        splitFields.forEach(splitField => {
+          value = value[splitField]
+        })
+
+        tablemon[field] = value
+      })
+
+      poketable.push(tablemon)
+
+    } else {
+      poketable.push(mon)
+    }
+  })
+
+  console.table(poketable)
+}
